82220b568306c73849105054e7230a18fc040a70
Leandro J. Capdesuñer Garcia <leandrojc88@gmail.com>
2023-07-15 23:59:29 -0300

    listo para mandar a firestore

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,72 +1,75 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-import { Request, Response } from "express";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {Request, Response} from "express";
 import * as functions from "firebase-functions";
-/*eslint-env es6*/
-const express = require('express');
-const fetch = require('node-fetch');
-const url = require('url');
+import * as express from "express";
+import fetch from "node-fetch";
+// import * as url from "url";
 
 const app = express();
 
-const appUrl = 'instafire-app.firebaseapp.com';
-const renderUrl = 'https://render-tron.appspot.com/render';
+const appUrl = "instafire-app.firebaseapp.com";
+// const renderUrl = "https://render-tron.appspot.com/render";
 
-// Generates the URL
-function generateUrl(request: any) {
+/**
+ * Generate url
+ * @param {any} request The first number.
+ * @return {string} The sum of the two numbers.
+ */
+/* function generateUrl(request: Request) {
   return url.format({
     protocol: request.protocol,
     host: appUrl,
-    pathname: request.originalUrl
+    pathname: request.originalUrl,
   });
-}
+} */
 
-// List of bots to target, add more if you'd like
-function detectBot(userAgent: any) {
-
+/**
+ * List of bots to target, add more if you"d like
+ * @param {string} userAgent browser agent
+ * @return {boolean}
+ */
+function detectBot(userAgent: string) {
   const bots = [
     // search engine crawler bots
-    'googlebot',
-    'bingbot',
-    'yandexbot',
-    'duckduckbot',
-    'slurp',
+    "googlebot",
+    "bingbot",
+    "yandexbot",
+    "duckduckbot",
+    "slurp",
     // social media link bots
-    'twitterbot',
-    'facebookexternalhit',
-    'linkedinbot',
-    'embedly',
-    'baiduspider',
-    'pinterest',
-    'slackbot',
-    'vkshare',
-    'facebot',
-    'outbrain',
-    'w3c_validator'
-  ]
+    "twitterbot",
+    "facebookexternalhit",
+    "linkedinbot",
+    "embedly",
+    "baiduspider",
+    "pinterest",
+    "slackbot",
+    "vkshare",
+    "facebot",
+    "outbrain",
+    "w3c_validator",
+  ];
 
 
   // Return true if the user-agent header matches a bot namespace
-  const agent = userAgent.toLowerCase()
+  const agent = userAgent.toLowerCase();
 
   for (const bot of bots) {
     if (agent.indexOf(bot) > -1) {
-      console.log('bot detected', bot, agent)
-      return true
+      console.log("bot detected", bot, agent);
+      return true;
     }
   }
 
-  console.log('no bots found')
-  return false
-
+  console.log("no bots found");
+  return false;
 }
 
-app.get('*', (req: Request, res: Response) => {
-
-  const isBot = detectBot(req.headers['user-agent']);
+app.get("*", (req: Request, res: Response) => {
+  const isBot = detectBot(req.headers["user-agent"] || "");
 
   if (isBot) {
-
-    const botUrl = generateUrl(req);
+    // const botUrl = generateUrl(req);
     // If Bot, fetch url via rendertron
 
     // fetch(`${renderUrl}/${botUrl}`)
@@ -75,8 +78,8 @@ app.get('*', (req: Request, res: Response) => {
 
     // Set the Vary header to cache the user agent, based on code from:
     // https://github.com/justinribeiro/pwa-firebase-functions-botrender
-    res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
-    res.set('Vary', 'User-Agent');
+    res.set("Cache-Control", "public, max-age=300, s-maxage=600");
+    res.set("Vary", "User-Agent");
 
     res.send(
       `
@@ -100,10 +103,9 @@ app.get('*', (req: Request, res: Response) => {
         <meta property="og:type" content="article">
         <meta property="og:site_name" content="AngularFirebase">
         <meta property="og:title" content="Home Page">
-        <meta property="og:description" content="Server side rendering that works with AngularFire2 - believe it">
+        <meta property="og:description" content="Server side rendering">
         <meta property="og:image" content="https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png">
         <meta property="og:url" content="https://instafire-app.firebaseapp.com">
-
 
       </head>
       <body>
@@ -111,22 +113,14 @@ app.get('*', (req: Request, res: Response) => {
       </body>
       </html>
 
-      `
-    )
-
-    // });
-
+      `);
   } else {
-
-    // Not a bot, fetch the regular Angular app
-    // This is not an infinite loop because Firebase Hosting Priorities dictate index.html will be loaded first
     fetch(`https://${appUrl}`)
       .then((res: any) => res.text())
       .then((body: any) => {
         res.send(body.toString());
-      })
+      });
   }
-
 });
 
 exports.app = functions.https.onRequest(app);
